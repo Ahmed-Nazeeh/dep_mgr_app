447763a604cb61b7e0eae1e4d5a0b0a6c32ead7e
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2022-03-03 14:39:06 +0200

    Update navbar, logo, syles, cards

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -144,4 +144,16 @@ class OrdersController < ApplicationController
         
     end 
     
+    def get_order_id
+      order = Order.where(id: 1)
+    
+      if order.exists?
+        order_id = Order.last.id + 1
+      else
+        order_id = 1
+      end
+      year_day = Date.today.yday
+      year = Date.today.year
+      "WO-#{year_day}-#{order_id}-#{year}"
+    end
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -25,18 +25,7 @@ def self.search(param)
     where("#{field_name} like ?", "%#{param}%")
   end
 # ------------------------------------------
-def get_order_id
-  order = Order.where(id: 1)
 
-  if order.exists?
-    order_id = Order.last.id + 1
-  else
-    order_id = 1
-  end
-  year_day = Date.today.yday
-  year = Date.today.year
-  "WO-#{year_day}-#{order_id}-#{year}"
-end
 
 # def get_last_ten_records
 #   Order.limit(10).order('id desc').reverse
